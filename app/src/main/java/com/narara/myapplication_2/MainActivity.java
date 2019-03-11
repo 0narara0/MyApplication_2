0c8c4fdff401c2539be6023d5f2d5c3472ae9364
miyoung <0narara0@gmail.com>
2019-03-11 13:50:38 +0900

    ActionBar, Menu 수정

--- app/src/main/java/com/narara/myapplication_2/MainActivity.java
+++ app/src/main/java/com/narara/myapplication_2/MainActivity.java
@@ -2,11 +2,11 @@ package com.narara.myapplication_2;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
--- app/src/main/java/com/narara/myapplication_2/activities/ParentActivity.java
+++ app/src/main/java/com/narara/myapplication_2/activities/ParentActivity.java
@@ -1,7 +1,9 @@
 package com.narara.myapplication_2.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.narara.myapplication_2.R;
 
@@ -11,5 +13,9 @@ public class ParentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
+    }
+
+    public void moveChildActivity(View view) {
+        startActivity(new Intent(this, ChildActivity.class));
     }
 }
